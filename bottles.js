084634f1636c9ecdc737fecdb11cdb5faa89bcc7
samar151110 <92369339+samar151110@users.noreply.github.com>
2022-11-08 14:10:46 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,46 @@
+img = "";
+Status = [];
+objects = [];
+function preload(){
+   img = loadImage("bottles.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(600, 600);
+    canvas.center();
+    object = ml5.objectDetector("cocossd", modals);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+
+function draw(){
+    image(video, 0, 0, 360, 360);
+    if(Status != ""){
+        object.detect(video, gotresult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("object").innerHTML = "Number of objects detected are : " + objects.length;
+            fill();
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 15);
+            noFill();
+            stroke();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+
+    }
+}
+
+function modelLoded(){
+    console.log("model is loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result){
+     if(error){
+        console.log(error);
+     }
+     console.log(result);
+     objects = results;
+}
